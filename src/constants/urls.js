1950059183fcb3082d930b02a347611cbad3efe4
Hadi Hosseinpour <hadi1998goodboy@gmail.com>
2022-01-15 17:23:12 +0330

    first

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,2 +1,2 @@
-export const apiUrl = "http://localhost:8000";
-//export const apiUrl = 'http://192.168.1.36/shop_laravel/public';
+//export const apiUrl = "http://localhost:8000";
+export const apiUrl = 'http://185.213.10.94:8000';
